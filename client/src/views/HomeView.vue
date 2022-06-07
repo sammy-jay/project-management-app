98205f875d8673d43caf799af263d03ac508fa5f
EC2 Default User <ec2-user@ip-172-31-9-123.ec2.internal>
2022-06-07 01:51:48 +0000

    vue study

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import { reactive } from 'vue'
+
+const state = reactive({ count: 0 })
+
+function increment() {
+  state.count++
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="bg-gray-300 p-3 mx-auto mt-5 rounded-md shadow-md w-[150px] flex flex-col items-center">
+        <p class="font-semibold text-5xl text-center">{{state.count}}</p>
+        <button class="rounded-full px-3 py-1 bg-green-400 text-grey-300 cursor-pointer mt-5 w-[90%]" @click="increment()">increment</button>
+    </div>
+
 </template>
